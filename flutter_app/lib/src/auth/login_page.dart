33810bb89a84d15420d54214dcaaeaa742522a82
mbashem <45259719+mbashem@users.noreply.github.com>
2023-10-21 14:22:21 +0600

    google sign in flutter

---
+++ flutter_app/lib/src/auth/login_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import './auth_repository.dart';
+
+class LoginPage extends StatefulWidget {
+  static const routeName = '/login';
+
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  Future<void> _handleSignIn() async {
+    print("handle sign in called");
+    try {
+      print("Calling");
+      final account = await signInWithGoogle();
+      print(account);
+      // Use account for user information, or proceed with Step 5.
+    } catch (error) {
+      print('Google Sign-In Error: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Sign-In Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _handleSignIn,
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
+  }
+}
